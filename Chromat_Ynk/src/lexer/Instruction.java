8aa67883a0031801e46549b53f0b402c943a24c3
MasterIncognito <106883795+MasterIncognito@users.noreply.github.com>
2024-05-17 16:33:54 +0200

    Began Lexer/Parser

---
+++ Chromat_Ynk/src/lexer/Instruction.java
@@ -0,0 +1,6 @@
+package lexer;
+
+public class Instruction {
+	private Command command;
+	private String[] args;
+}
---
+++ Chromat_Ynk/src/lexer/Parser.java
@@ -0,0 +1,11 @@
+package lexer;
+
+public class Parser {
+	private Instruction[] instructions;
+	public void Lexer (String[] path) {
+		/**Lire le fichier
+		 * 
+		 * mettre la liste d'instructions dans instructions
+		 * **/
+	}
+}
